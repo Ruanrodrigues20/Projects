a1c44e074529b5dd2e60bcfad41a0de1c828efee
Ruan Rodrigues <minip0607@gmail.com>
2024-05-17 16:15:40 -0300

    upload

---
+++ Jogo da forca/bcd.py
@@ -0,0 +1,5 @@
+palavras = [
+    'Gato', 'Cão', 'Casa', 'Livro', 'Sol', 'Lua', 'Flor', 'Pato', 'Rosa', 'Mãe',
+    'Pão', 'Chuva', 'Mar', 'Água', 'Peixe', 'Vento', 'Luz', 'Fruta', 'Branco', 'Bola',
+    'Lápis', 'Mala', 'Rato', 'Café', 'Mesa', 'Copo', 'Cama', 'Fogo', 'Piso', 'Porta'
+]
--- Jogo da forca/jogo.py
+++ Jogo da forca/jogo.py
@@ -1,43 +1,86 @@
-palavra = 'RUAN'
-car = [i for i in palavra]
-tentativas = 6
-acertos = 0
-saida = ['_' for i in range(len(car))]
-letras = []
+from bcd import palavras
+from random import choice
 
-while acertos < len(car) or tentativas > 0:
-    if acertos >= len(car): break
-    ver = acertos
-    entrada_0 = [i for i in input('Diga letra/palavra: ').upper()]
-   
-    if len(entrada_0) <= len(car):
-        for i in range(len(entrada_0)):
-            for j in range(len(car)):
-                if entrada_0[i] == car[j]:
-                    if j > 0:
-                        saida[j] = car[j].lower()
-                    elif j == 0:
-                        saida[j] = car[j]
-                
-            if not entrada_0[i] in letras and entrada_0[i] in car:
-                acertos += 1
-            letras.append(entrada_0[i])
+def dificuldade():
+    # verifica qual o nível de dificuldade da palavra que o jogador escolheu e
+    # retorna uma string adequada ao nivel escolhido
+    
+    para = False
+    while not para:
+        d = input('Qual o nível de dificuldade [fácil, medio, díficl]? ').lower()
+        if d[0] == 'f':
+            palavra = choice(palavras).lower()
+            para = True
+        elif d[0] == 'm':
+            palavra = choice(palavras).lower()
+            para = True
+        elif d[0] == 'd':
+            palavra = choice(palavras).lower()
+            para = True
+    return palavra
 
-    if ver >= acertos:
-        tentativas -= 1
-        print('Errou!')
+
+def forca():
+    palavra = dificuldade()
+
+    car = [i for i in palavra]
+    tentativas = 6
+    acertos = 0
+    saida = ['_' for i in range(len(car))]
+    letras = []
+
+    while acertos < len(car) and tentativas > 0:
+        entrada_0 = input('Diga letra/palavra: ').lower()
+
+        # Verifica se o usuário adivinhou a palavra inteira
+        if len(entrada_0) > 1:
+            if list(entrada_0) == car:
+                saida = car
+                acertos = len(car)
+                break
+            else:
+                tentativas -= 1
+                print('Errou!')
         
-    print('')
-    print(saida)
-    print(f'Você tem {acertos} acertos e {tentativas} tentativas')
-    print('')
+        # Verifica se o usuário adivinhou uma letra
+        else:
+            letra = entrada_0[0]
+            if letra in letras:
+                print('Você já tentou essa letra.')
+            else:
+                letras.append(letra)
+                if letra in car:
+                    for j in range(len(car)):
+                        if letra == car[j]:
+                            saida[j] = car[j]
+                            acertos += 1
+                else:
+                    tentativas -= 1
+                    print('Errou!')
+        
+        print('')
+        print(' '.join(saida))
+        print(f'Você tem {acertos} acertos e {tentativas} tentativas')
+        print('')
 
-
-if acertos == len(car):
-    print('PARABENS, você conseguiu')
-else:
-    print('TENTE DA PRÓXIMA VEZ')
+    if acertos == len(car):
+        print('PARABÉNS, você conseguiu!')
+    else:
+        print('TENTE DA PRÓXIMA VEZ')
     
+    print(' ')
 
 
-    
+def main():
+    print('Jogo da forca')
+    continuar = 's'
+    while continuar == 's':
+        forca()
+        cont = input('Mais uma rodada [s/n]? ').lower()
+        continuar = cont[0]
+        print(' ')
+
+    print('Até a próxima!')
+
+
+main()
